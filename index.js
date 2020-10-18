3932bd782b4dc9ff7c0f23fa716effe7c9c2e5ec
Niles87 <nbing19@gmail.com>
2020-10-17 19:03:00 -0700

    basic nodejs docker setup

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
-app.get("/", (req, res) => res.send("Jake from State Farm"));
+app.get("/", (req, res) =>
+  res.json([
+    { name: "Mary", email: "mary@email.com" },
+    { name: "Bob", email: "bob@email.com" },
+  ])
+);
 
 app.listen(PORT, () => console.log(`App running on port: ${PORT}`));
